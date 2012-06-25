15c646a80bfa7bcb0ea293201fef624814641bf9
David Howells <dhowells@redhat.com>
2012-06-25 12:55:18 +0100

    VFS: Make clone_mnt()/copy_tree()/collect_mounts() return errors
    copy_tree() can theoretically fail in a case other than ENOMEM, but always
    returns NULL which is interpreted by callers as -ENOMEM.  Change it to return
    an explicit error.
    
    Also change clone_mnt() for consistency and because union mounts will add new
    error cases.
    
    Thanks to Andreas Gruenbacher <agruen@suse.de> for a bug fix.
    [AV: folded braino fix by Dan Carpenter]
    
    Original-author: Valerie Aurora <vaurora@redhat.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Cc: Valerie Aurora <valerie.aurora@gmail.com>
    Cc: Andreas Gruenbacher <agruen@suse.de>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    (cherry picked from commit be34d1a3bc4b6f357a49acb55ae870c81337e4f0)

--- fs/namespace.c
+++ fs/namespace.c
@@ -750,7 +750,7 @@ static struct mount *clone_mnt(struct mount *old, struct dentry *root,
 	list_add_tail(&mnt->mnt_instance, &sb->s_mounts);
 	br_write_unlock(&vfsmount_lock);
 
-		if ((flag & CL_SLAVE) ||
+	if ((flag & CL_SLAVE) ||
 		    ((flag & CL_SHARED_TO_SLAVE) && IS_MNT_SHARED(old))) {
 		list_add(&mnt->mnt_slave, &old->mnt_slave_list);
 		mnt->mnt_master = old;
