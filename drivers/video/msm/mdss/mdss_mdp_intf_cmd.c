a623c642a896d4aba5f758b1171d8967e24d4bab
Kuogee Hsieh <khsieh@codeaurora.org>
2014-05-01 16:02:48 -0700

    mdss: display: add support of dual dsi partial update.
    This patch adds l_roi and r_roi fields to display commit struct to
    support partial update for dual DSI panels. Panel side
    frame buffer's column, page address and DSI controller's
    stream size are updated based on the ROI requested.
    
    Change-Id: I70957c9ee1546448416441a8d0608fb00c3b92ca
    Signed-off-by: Jeykumar Sankaran <jsanka@codeaurora.org>
    Signed-off-by: Siddhartha Agrawal <agrawals@codeaurora.org>
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -413,7 +413,8 @@ static void clk_ctrl_work(struct work_struct *work)
 static int mdss_mdp_cmd_add_vsync_handler(struct mdss_mdp_ctl *ctl,
 		struct mdss_mdp_vsync_handler *handle)
 {
-	struct mdss_mdp_cmd_ctx *ctx;
+	struct mdss_mdp_ctl *sctl = NULL;
+	struct mdss_mdp_cmd_ctx *ctx, *sctx = NULL;
 	unsigned long flags;
 	bool enable_rdptr = false;
 
@@ -425,6 +426,9 @@ static int mdss_mdp_cmd_add_vsync_handler(struct mdss_mdp_ctl *ctl,
 
 	MDSS_XLOG(ctl->num, ctx->koff_cnt, ctx->clk_enabled,
 					ctx->rdptr_enabled);
+	sctl = mdss_mdp_get_split_ctl(ctl);
+	if (sctl)
+		sctx = (struct mdss_mdp_cmd_ctx *) sctl->priv_data;
 
 	spin_lock_irqsave(&ctx->clk_lock, flags);
 	if (!handle->enabled) {
@@ -432,13 +436,19 @@ static int mdss_mdp_cmd_add_vsync_handler(struct mdss_mdp_ctl *ctl,
 		list_add(&handle->list, &ctx->vsync_handlers);
 
 		enable_rdptr = !handle->cmd_post_flush;
-		if (enable_rdptr)
+		if (enable_rdptr) {
 			ctx->vsync_enabled++;
+			if (sctx)
+				sctx->vsync_enabled++;
+		}
 	}
 	spin_unlock_irqrestore(&ctx->clk_lock, flags);
 
-	if (enable_rdptr)
+	if (enable_rdptr) {
 		mdss_mdp_cmd_clk_on(ctx);
+		if (sctx)
+			mdss_mdp_cmd_clk_on(sctx);
+	}
 
 	return 0;
 }
@@ -446,7 +456,8 @@ static int mdss_mdp_cmd_add_vsync_handler(struct mdss_mdp_ctl *ctl,
 static int mdss_mdp_cmd_remove_vsync_handler(struct mdss_mdp_ctl *ctl,
 		struct mdss_mdp_vsync_handler *handle)
 {
-	struct mdss_mdp_cmd_ctx *ctx;
+	struct mdss_mdp_ctl *sctl;
+	struct mdss_mdp_cmd_ctx *ctx, *sctx = NULL;
 	unsigned long flags;
 
 	ctx = (struct mdss_mdp_cmd_ctx *) ctl->priv_data;
@@ -457,6 +468,9 @@ static int mdss_mdp_cmd_remove_vsync_handler(struct mdss_mdp_ctl *ctl,
 
 	MDSS_XLOG(ctl->num, ctx->koff_cnt, ctx->clk_enabled,
 				ctx->rdptr_enabled, 0x88888);
+	sctl = mdss_mdp_get_split_ctl(ctl);
+	if (sctl)
+		sctx = (struct mdss_mdp_cmd_ctx *) sctl->priv_data;
 
 	spin_lock_irqsave(&ctx->clk_lock, flags);
 	if (handle->enabled) {
@@ -464,8 +478,11 @@ static int mdss_mdp_cmd_remove_vsync_handler(struct mdss_mdp_ctl *ctl,
 		list_del_init(&handle->list);
 
 		if (!handle->cmd_post_flush) {
-			if (ctx->vsync_enabled)
+			if (ctx->vsync_enabled) {
 				ctx->vsync_enabled--;
+				if (sctx)
+					sctx->vsync_enabled--;
+			}
 			else
 				WARN(1, "unbalanced vsync disable");
 		}
@@ -603,17 +620,41 @@ static void mdss_mdp_cmd_set_sync_ctx(
 
 static int mdss_mdp_cmd_set_partial_roi(struct mdss_mdp_ctl *ctl)
 {
+	struct mdss_mdp_ctl *sctl = NULL;
+	struct mdss_rect *roi;
 	int rc = 0;
-	if (ctl->roi.w && ctl->roi.h && ctl->roi_changed &&
-			ctl->panel_data->panel_info.partial_update_enabled) {
-		ctl->panel_data->panel_info.roi_x = ctl->roi.x;
-		ctl->panel_data->panel_info.roi_y = ctl->roi.y;
-		ctl->panel_data->panel_info.roi_w = ctl->roi.w;
-		ctl->panel_data->panel_info.roi_h = ctl->roi.h;
 
-		rc = mdss_mdp_ctl_intf_event(ctl,
-				MDSS_EVENT_ENABLE_PARTIAL_UPDATE, NULL);
+	if (!ctl->panel_data->panel_info.partial_update_enabled)
+		return rc;
+
+	sctl = mdss_mdp_get_split_ctl(ctl);
+
+	/* save roi to pinfo which used by dsi controller */
+	roi = &ctl->panel_data->panel_info.roi;
+	*roi = ctl->roi;
+
+	if (sctl) {
+		/* save roi to pinfo whcih used by dsi controller */
+		roi = &sctl->panel_data->panel_info.roi;
+		*roi = sctl->roi;
 	}
+
+	/* set panel col and page addr */
+	rc = mdss_mdp_ctl_intf_event(ctl,
+			MDSS_EVENT_ENABLE_PARTIAL_ROI, NULL);
+	return rc;
+}
+
+static int mdss_mdp_cmd_set_stream_size(struct mdss_mdp_ctl *ctl)
+{
+	int rc = 0;
+
+	if (!ctl->panel_data->panel_info.partial_update_enabled)
+		return rc;
+
+	/* set dsi controller stream size */
+	rc = mdss_mdp_ctl_intf_event(ctl,
+			MDSS_EVENT_DSI_STREAM_SIZE, NULL);
 	return rc;
 }
 
@@ -657,6 +698,7 @@ static int mdss_mdp_cmd_panel_on(struct mdss_mdp_ctl *ctl,
 
 int mdss_mdp_cmd_kickoff(struct mdss_mdp_ctl *ctl, void *arg)
 {
+	struct mdss_mdp_ctl *sctl;
 	struct mdss_mdp_cmd_ctx *ctx;
 	unsigned long flags;
 
@@ -670,6 +712,10 @@ int mdss_mdp_cmd_kickoff(struct mdss_mdp_ctl *ctl, void *arg)
 		pr_err("ctx=%d stopped already\n", ctx->pp_num);
 		return -EPERM;
 	}
+
+	sctl = mdss_mdp_get_split_ctl(ctl);
+	if (sctl)
+		sctx = (struct mdss_mdp_cmd_ctx *) sctl->priv_data;
 
 	mdss_mdp_ctl_perf_set_transaction_status(ctl,
 		PERF_HW_MDP_STATE, PERF_STATUS_BUSY);
@@ -701,6 +747,8 @@ int mdss_mdp_cmd_kickoff(struct mdss_mdp_ctl *ctl, void *arg)
 	mdss_mdp_ctl_intf_event(ctl, MDSS_EVENT_DSI_CMDLIST_KOFF, NULL);
 
 	mdss_mdp_cmd_set_sync_ctx(ctl, NULL);
+
+	mdss_mdp_cmd_set_stream_size(ctl);
 
 	mdss_mdp_irq_enable(MDSS_MDP_IRQ_PING_PONG_COMP, ctx->pp_num);
 	mdss_mdp_ctl_write(ctl, MDSS_MDP_REG_CTL_START, 1);
