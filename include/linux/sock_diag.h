2d721be13975869b91726ebf179de61ac41f1127
Lorenzo Colitti <lorenzo@google.com>
2015-12-16 12:30:03 +0900

    net: diag: Add the ability to destroy a socket.
    This patch adds a SOCK_DESTROY operation, a destroy function
    pointer to sock_diag_handler, and a diag_destroy function
    pointer.  It does not include any implementation code.
    
    [Backport of net-next 64be0aed59ad519d6f2160868734f7e278290ac1]
    
    Change-Id: I3db262a7e41f1f8452ff0968d4001234598190d8
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/sock_diag.h
+++ include/linux/sock_diag.h
@@ -44,6 +44,7 @@ int sock_diag_check_cookie(void *sk, __u32 *cookie);
 void sock_diag_save_cookie(void *sk, __u32 *cookie);
 
 int sock_diag_put_meminfo(struct sock *sk, struct sk_buff *skb, int attr);
+int sock_diag_destroy(struct sock *sk, int err);
 
 int sock_diag_destroy(struct sock *sk, int err);
 
