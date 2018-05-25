2d6512f3c26ecc93a230246ab5bf37706d113e5b
D. Andrei Măceș <andrei@unlegacy-android.org>
2018-05-25 10:33:59 +0300

    Revert "power: power_supply: move POWER_SUPPLY_PROP_USB_HC to type 'int' order"
    This reverts commit 4faf8ddc988cf2b43af9b0801adc973bcc36d407.
    
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -185,8 +185,6 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(time_to_full_avg),
 	POWER_SUPPLY_ATTR(type),
 	POWER_SUPPLY_ATTR(scope),
-	/* Local extensions */
-	POWER_SUPPLY_ATTR(usb_hc),
 	POWER_SUPPLY_ATTR(system_temp_level),
 	POWER_SUPPLY_ATTR(resistance),
 	/* Local extensions */
